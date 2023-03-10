ffbb083bd3929700fd0406c64feab926dc6030df
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-03-10 18:10:32 +0530

    used axios for using created node backend apis

--- expenseTracker.js
+++ expenseTracker.js
@@ -7,19 +7,19 @@ const msg = document.querySelector('.msg');
 
 myForm.addEventListener('submit', onSubmit);
 
-var count = 0;
-// retrieving stored usersDetails from local storage
-var totalExpenseCount = localStorage.length;
-if(totalExpenseCount!=0){
-    count = +Object.keys(localStorage)[0];
-    let i = +Object.keys(localStorage)[totalExpenseCount-1];
-    while(i<=count){
-        let expenseObj_deserialized = JSON.parse(localStorage.getItem(i));
-        console.log(expenseObj_deserialized);
-        showUserOnScreen(expenseObj_deserialized);
-        i++;
-    }
-}
+// retrieving stored expenses when DOM loads
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get('http://localhost:3000/expenseData')
+    .then((response) => {
+        for(expenseObj of response.data){
+            showUserOnScreen(expenseObj);
+        }
+    })
+    .catch(err => {
+        myForm.innerHTML = '<h1>Error: Something went wrong!!!!</h1>';
+        console.log(err);
+    })
+})
 
 function onSubmit(e){
     e.preventDefault();
@@ -30,29 +30,43 @@ function onSubmit(e){
         setTimeout(() => msg.remove(), 3000);
     }
     else{
-        var totalExpenseCount = localStorage.length;
-        if(totalExpenseCount==0){
-            count=0;
-        }else{
-            count = +Object.keys(localStorage)[0];
-        }
-        count++;
-
         let expenseObj = {
-            expenseId: count,
-            expenseDesc : expenseDesc.value,
+            description : expenseDesc.value,
             amount : expenseAmount.value,
             category: category.value,
         }
-        let expenseObj_serialized = JSON.stringify(expenseObj);
-        localStorage.setItem(count, expenseObj_serialized);
-        showUserOnScreen(expenseObj);
+        if(document.querySelector('#submitBtn').value === 'Update'){
+            const expenseId = document.querySelector('#expenseId').value;
+            axios
+              .put('http://localhost:3000/expenseData/'+expenseId, expenseObj)
+              .then((response) => {
+                showUserOnScreen(response.data);
+              })
+              .catch((err) => {
+                document.body.innerHTML += "Error: Something went wrong!!!!";
+                console.log(err);
+              });
+
+        }
+        else{
+            axios.post('http://localhost:3000/expenseData', expenseObj)
+            .then((response) => {
+                showUserOnScreen(response.data);
+            })
+            .catch(err => {
+                document.body.innerHTML += 'Error: Something went wrong!!!!';
+                console.log(err)
+            });
+        }
+        expenseDesc.value = '';
+        expenseAmount.value = '';
+        category.value = '';
     }
 }
 
 function showUserOnScreen(obj){
     const li = document.createElement('li');
-    li.appendChild(document.createTextNode(`${obj.expenseDesc} : ${obj.amount} : ${obj.category}`));
+    li.appendChild(document.createTextNode(`${obj.description} : ${obj.amount} : ${obj.category}`));
 
     // create delete btn element
     var delBtn = document.createElement('button');
@@ -69,18 +83,26 @@ function showUserOnScreen(obj){
     // delete event
     delBtn.onclick = () =>{
         if(confirm('Are you sure ?')){
-            expenseList.removeChild(li);
-            localStorage.removeItem(obj.expenseId);
+            axios
+              .delete("http://localhost:3000/expenseData/" + obj.id)
+              .then((response) => expenseList.removeChild(li))
+              .catch((err) => console.log(err));
         }
     }
 
     // edit event
     edtBtn.onclick = () =>{
         expenseList.removeChild(li);
-        localStorage.removeItem(obj.expenseId);
-        expenseDesc.value = obj.expenseDesc;
+        expenseDesc.value = obj.description;
         expenseAmount.value = obj.amount;
         category.value = obj.category;
+        const idElem = document.createElement('input');
+        idElem.type = 'hidden';
+        idElem.id = 'expenseId';
+        idElem.value = obj.id;
+        myForm.children[6].children[0].value = 'Update';
+        myForm.appendChild(idElem);
+
     }
     
     li.appendChild(delBtn);
