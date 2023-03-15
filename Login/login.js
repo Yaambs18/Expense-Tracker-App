7adf304628923ca895c4fa02100ed14bf0f17de5
Yaambs18 <yanshbhardwaj14@gmail.com>
2023-03-15 15:07:16 +0530

    added forgot Password Functionality

--- Login/login.js
+++ Login/login.js
@@ -21,3 +21,25 @@ async function login(e) {
         console.log(error);
     }
 }
+
+async function forgotPassword(e){
+    e.preventDefault();
+    let loginDetails = {
+        email: e.target.email.value
+    };
+    try{
+        const res = await axios.post('http://localhost:3000/password/forgotpassword', loginDetails);
+        if(res.status === 200){
+            alert(res.data.message);
+            window.location.href = './login.html';
+        }
+        else{
+            console.log(res);
+            throw new Error(res);
+        }
+    }
+    catch(error) {
+        document.body.innerHTML += `<h1>Error: ${error.message}</h1>`;
+        console.log(error);
+    }
+}
